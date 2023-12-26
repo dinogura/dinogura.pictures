dd192a96e6d2567d7a0e01cbd15c67893ec0eee6
dinogura <hi@dinogura.pictures>
2023-12-26 09:05:11 +0700

    simplify

---
+++ js/main.js
@@ -0,0 +1,53 @@
+{"props":{"pageProps":{"data":
+{"_id":"657d3c3350bb58bdd28191bc",
+"linkId":29247888,
+"title":"Dino Gura",
+"slug":"dinogura",
+"theme":"canon-blue",
+"style":{"backgroundImage":"","backgroundBlur":"base","backgroundDark":"base","backgroundColor":"#063869","backgroundStyle":"base","textColor":"#D9E4E8","font":"base","buttonBackgroundColor":"#0C8BC1","buttonTextColor":"#0C8BC1","buttonType":"elevate","componentPlacement":"start","buttonColor":"","buttonBorderColor":"","buttonIconBackground":"","buttonShadowColor":"","buttonSize":""},
+"gtag":null,"metaImage":"943bbd3a-86ad-43b0-9116-28ddbf3950b6_980x980.png",
+"description":"Dino Gura Pictures. Fan account of Gawr Dura. New post of #DinoGura every Monday and Wednesday. Managed by Maarief S. Indrawan",
+
+"components":[
+
+{"_id":"657d3c3350bb58bdd28191bd","micrositeId":"657d3c3350bb58bdd28191bc","active":true,"kind":"PROFILE","data":{"image":"5f3244b2-f6a4-43c5-8ba7-5f6b7473b23c_400x400.webp.jpeg","imageBackground":"91c91954-31d9-40b5-8234-7b5e3f2a5017_980x380.webp.jpeg","profileBackground":"#687232","backgroundColor":"#2898cf","profileStyle":"square","title":"Dino Gura","titleFontSize":"lg","titleIsBold":true,"titleIsItalic":false,"description":"Fan account of Gawr Gura ⁣⁣\nSharing cute photo of #dinogura\nNew post every Monday and Wednesday⁣","descriptionFont":"red-hat-display","descriptionIsBold":false,"descriptionIsItalic":false},"_created":"2023-12-16T05:57:07.757Z","_modified":"2023-12-23T01:36:53.350Z","version":4,"tag":"","index":0},
+
+{"_id":"6588415438beadd721762dd4","micrositeId":"657d3c3350bb58bdd28191bc","active":true,"kind":"HORIZONTAL_LINK","data":{"style":"square","backgroundColor":"#16a74a","links":[{"icon":"facebook","url":"https://www.facebook.com/dinogura.pictures"},{"icon":"twitter","url":"https://twitter.com/dinogura_pics"},{"icon":"instagram","url":"https://www.instagram.com/dinogura.pictures/"},{"icon":"mail","url":"hi@dinogura.pictures"}]},"_created":"2023-12-24T14:33:56.553Z","_modified":"2023-12-24T14:40:49.132Z","version":4,"tag":"","index":1},
+
+{"_id":"658639db1b034f996f595be2","micrositeId":"657d3c3350bb58bdd28191bc","active":true,"kind":"TEXT","data":{"textPosition":"center","textSize":"base","textFont":"base","textSizeCustom":0,"content":"Next Post","isSpoiler":false,"spoilerButtonText":"","spoilerButtonTextPosition":"","spoilerButtonIcon":"","spoilerButtonColor":"","spoilerButtonFont":"","spoilerBackground":"","spoilerBackgroundOpacity":"","spoilerButtonBorderColor":"","spoilerButtonIconBackground":"","spoilerButtonShadowColor":"","password":"","passwordActive":false},"_created":"2023-12-23T01:37:31.186Z","_modified":"2023-12-23T01:39:22.704Z","version":4,"tag":"","index":2},
+
+{"_id":"658639c038beadd72175c684","micrositeId":"657d3c3350bb58bdd28191bc","active":true,"kind":"COUNTDOWN","data":{"timestamp":1703638800},"_created":"2023-12-23T01:37:04.051Z","_modified":"2023-12-23T01:37:24.095Z","version":4,"tag":"","index":3},
+
+{"_id":"65863a511b034f996f595c06","micrositeId":"657d3c3350bb58bdd28191bc","active":true,"kind":"DIVIDER","data":{"dividerStyle":"base","dividerHeight":3,"dividerBorderColor":""},"_created":"2023-12-23T01:39:29.965Z","_modified":"2023-12-23T01:39:29.965Z","version":2,"tag":"","index":4},
+
+{"_id":"6586379238beadd72175c5e1","micrositeId":"657d3c3350bb58bdd28191bc","active":true,"kind":"IMAGE_SLIDE",
+"data":{"images":[
+{"id":"6XVGggd42SCz","image":"b53caafd-16de-482d-88f1-88c8a03e346e_980x980.png","active":false},
+{"id":"o62zll9xurZe","image":"ed45198b-b894-4b48-b2f4-949b9df7dd74_980x980.png","active":false},
+{"id":"uifQTMOo8AaB","image":"b8f6aaa5-c7bd-43bc-bf83-b68a02d760c4_980x980.png","active":false},
+{"id":"qSE1JaDyNPzS","image":"eb267745-a688-44d4-aa89-7a5d7f0752ea_980x980.png","active":false},
+{"id":"vB1d16gFKW-S","image":"7285fe03-c7f4-41ee-a119-165dcff75d10_980x980.png","active":false},
+{"id":"uSJZ-XNXeT4s","image":"a207480c-48b1-40dd-8f93-b0f4e41cad8e_980x980.png","active":false},
+{"id":"9GIX7HQ2JyCR","image":"04de0091-f21b-433a-b536-c39d893706ba_980x980.png","active":false},
+{"id":"WtUtyA4eyNAg","image":"5746dfd8-4d27-49c3-88c0-07c57bbbafc2_980x980.png","active":false},
+{"id":"AopsebA_ZXOx","image":"c5511a0a-fc16-4d6a-bd30-6fae4c8c5739_980x980.png","active":false},
+{"id":"sDE1M0a5Sgag","image":"9f904ef8-370a-44a3-88bb-bb8d6fa7999e_980x980.png","active":false}],
+"style":"default","isSpoiler":true,"spoilerButtonText":"Post 1 -10","spoilerButtonTextPosition":"","spoilerButtonIcon":"45e474a0-1c3d-4c08-8adb-713ddc9f262b_764x764.png","spoilerButtonColor":"","spoilerButtonFont":"","spoilerBackground":"","spoilerBackgroundOpacity":"","spoilerButtonBorderColor":"","spoilerButtonIconBackground":"","spoilerButtonShadowColor":"","password":"","passwordActive":false},"_created":"2023-12-23T01:27:46.072Z","_modified":"2023-12-23T01:36:13.932Z","version":4,"tag":"","index":5},
+
+{"_id":"65863a623eba0cdad82a3ad7","micrositeId":"657d3c3350bb58bdd28191bc","active":true,"kind":"IMAGE_SLIDE",
+"data":{"images":[
+{"id":"4l9TublCwpns","image":"bf2c2a54-6dea-44df-a142-1ea980961b02_931x931.png","active":false},
+{"id":"PBu_H5e5FHr0","image":"b7d23026-3a39-404e-b6b4-1867ab30d63e_980x980.png","active":false},
+{"id":"JXnXd7Jjcpid","image":"76aeecd4-6860-47cf-a44c-f3c142aea688_980x980.png","active":false},
+{"id":"q4IxEOB415IG","image":"74d12542-8ed9-4e26-a800-a541932f0ca5_2160x2160.webp.png","active":false},
+{"id":"0rXMRosauwkS","image":"cb12a539-fcdc-4426-aae2-8d06a63c4e17_2160x2160.webp.png","active":false},
+{"id":"q4IxEOB415IG","image":"74d12542-8ed9-4e26-a800-a541932f0ca5_2160x2160.webp.png","active":false},
+{"id":"0rXMRosauwkS","image":"cb12a539-fcdc-4426-aae2-8d06a63c4e17_2160x2160.webp.png","active":false},
+{"id":"q4IxEOB415IG","image":"74d12542-8ed9-4e26-a800-a541932f0ca5_2160x2160.webp.png","active":false},
+{"id":"0rXMRosauwkS","image":"cb12a539-fcdc-4426-aae2-8d06a63c4e17_2160x2160.webp.png","active":false},
+{"id":"q4IxEOB415IG","image":"74d12542-8ed9-4e26-a800-a541932f0ca5_2160x2160.webp.png","active":false}],
+"style":"default","isSpoiler":true,"spoilerButtonText":"Post 11 - 20","spoilerButtonTextPosition":"","spoilerButtonIcon":"3955483c-8dba-48b8-b34a-8c791f946e2a_384x384.png","spoilerButtonColor":"","spoilerButtonFont":"","spoilerBackground":"","spoilerBackgroundOpacity":"","spoilerButtonBorderColor":"","spoilerButtonIconBackground":"","spoilerButtonShadowColor":"","password":"","passwordActive":false},"_created":"2023-12-23T01:39:46.486Z","_modified":"2023-12-24T14:37:14.681Z","version":4,"tag":"","index":6}
+
+],
+
+"extras":{"hideAds":null,"hideFooter":null,"vendor":"daceni"},"hideFooter":true,"modified":"e_1703295685"},"hideFooter":true,"hideAds":true},"__N_SSP":true},"page":"/[[...any]]","query":{},"buildId":"SgC88C_6LaSr9oCmijauS","assetPrefix":"https://microsite.s.id","isFallback":false,"gssp":true,"locale":"en","locales":["en","id"],"defaultLocale":"en","scriptLoader":[]}
